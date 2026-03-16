e1cb440e32c67b23b1bfe6111f4cd53883055bb5
sudeep-dev-cse <sudeepyad21@gmail.com>
2026-03-16 22:19:12 +0530

    Update two_sum.cpp

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -2,14 +2,18 @@
 #include <unordered_map>
 using namespace std;
 
-vector<int> twoSum(vector<int>& nums, int target) {
-    unordered_map<int,int> mp;
-    for(int i=0;i<nums.size();i++){
-        int complement = target - nums[i];
-        if(mp.count(complement)){
-            return {mp[complement], i};
-        }
-        mp[nums[i]] = i;
+  vector<int> twoSum(vector<int>& nums, int target) {
+       unordered_map<int ,int>m;
+            vector<int> ans;
+       for(int i = 0;i < nums.size(); i++){
+        int first = nums[i];
+         int second = target - first;
+         if(m.find(second) != m.end()){
+           ans.push_back(i);
+            ans.push_back(m[second]);
+            break;
+         }
+         m[first] = i;
+       }
+      return ans;
     }
-    return {};
-}
